38d7efa7e154ecb17d765d9e1bc793290696659b
Hanzala-Arif <137094281+Hanzala-Arif@users.noreply.github.com>
2024-12-26 14:05:27 +0500

    Base Setup

--- app/src/test/java/com/example/myapp/AnonymousFunction.kt
+++ app/src/test/java/com/example/myapp/AnonymousFunction.kt
@@ -1,12 +1,30 @@
 package com.example.myapp
 
-fun main()
-{
-    val list= listOf(1,2,3,4,5,6)
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5, 6)
     list.forEach {
         println(it.toString())
     }
-}`
-val name=fun(firstName:String,lastName:String):String{
+    println(name("Haznala", "Arif"))
 
+    val persondata: (String, Int) -> Pair<String, Int> = { name, age ->
+        Pair(name.uppercase(), age + 1)
+    }
+
+    val result = person("Hanzala", 24, persondata)
+    println(result)
+}
+
+//Explicit anonymous function
+val name = fun(firstName: String, lastName: String): List<String> {
+    return listOf(firstName, lastName)
+}
+
+//Higher Order Function
+private fun person(
+    name: String,
+    age: Int,
+    Person: (String, Int) -> Pair<String, Int>
+): Pair<String, Int> {
+    return Person(name, age)
 }
